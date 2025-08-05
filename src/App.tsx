75b23ba71c5c8768243200c0ec7ec7447edbbb79
Lukasz <lukasz.maniak22@gmail.com>
2025-08-05 20:00:37 +0200

    firstProjectSetup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl text-green-400">Card War</div>
+    </>
+  );
+}
+
+export default App;
